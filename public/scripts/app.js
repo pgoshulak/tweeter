8e1c2b9206be0235fb62d3d409c04bcb3a16d5aa
Peter Goshulak <pgoshulak@gmail.com>
2018-04-25 13:09:57 -0400

    Load and sort tweets via ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,53 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 function createTweetElement(tweetData) {
   let $tweet = $('<article></article>').addClass('tweet');
 
@@ -118,6 +71,16 @@ function renderTweets(data) {
   }
 }
 
+function loadTweets() {
+  return $.get('/tweets');
+}
+
+function sortTweetsAscending(data) {
+  return data.sort((a,b) => {
+    return b.created_at - a.created_at;
+  });
+}
+
 function submitTweet(formData) {
   console.log(formData)
   return $.post('/tweets', formData)
@@ -131,7 +94,12 @@ function clearComposer() {
 }
 
 $(document).ready(() => {
-  renderTweets(data);
+  loadTweets()
+    .then(sortTweetsAscending)
+    .then(renderTweets)
+    .fail((err) => {
+      console.error('Error: ',err)
+    });
 
   $('#composer').on('submit', (e) => {
     e.preventDefault();
